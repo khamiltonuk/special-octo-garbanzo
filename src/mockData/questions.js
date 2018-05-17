fb31ffae7f944fd4846d67187c2c75bf8906121f
khamiltonuk <krisham87@hotmail.com>
2018-05-17 09:15:40 +0200

    add mock data

---
+++ src/mockData/questions.js
@@ -0,0 +1,474 @@
+const questions = [
+  {
+    url: "/questions/9057",
+    published_at: "2018-05-17T04:37:08.170459+00:00",
+    question: "Favourite programming language?",
+    choices: [
+      {
+        url: "/questions/9057/choices/34169",
+        choice: "Objective-C",
+        votes: 0
+      },
+      {
+        url: "/questions/9057/choices/34168",
+        choice: "Python",
+        votes: 0
+      },
+      {
+        url: "/questions/9057/choices/34170",
+        choice: "Ruby",
+        votes: 0
+      },
+      {
+        url: "/questions/9057/choices/34167",
+        choice: "Swift",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/10",
+    published_at: "2015-05-27T21:22:26.670000+00:00",
+    question: "Game Genre",
+    choices: [
+      {
+        url: "/questions/10/choices/69",
+        choice: "Action",
+        votes: 4
+      },
+      {
+        url: "/questions/10/choices/71",
+        choice: "Action-adventure",
+        votes: 4
+      },
+      {
+        url: "/questions/10/choices/70",
+        choice: "Shooter",
+        votes: 4
+      },
+      {
+        url: "/questions/10/choices/74",
+        choice: "Strategy",
+        votes: 4
+      },
+      {
+        url: "/questions/10/choices/72",
+        choice: "Role-playing",
+        votes: 1
+      },
+      {
+        url: "/questions/10/choices/73",
+        choice: "Simulation",
+        votes: 1
+      },
+      {
+        url: "/questions/10/choices/75",
+        choice: "Sports",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/9",
+    published_at: "2015-05-27T21:22:26.648000+00:00",
+    question: "Favourite hot beverage?",
+    choices: [
+      {
+        url: "/questions/9/choices/67",
+        choice: "Apple Cider",
+        votes: 2
+      },
+      {
+        url: "/questions/9/choices/65",
+        choice: "Tea",
+        votes: 2
+      },
+      {
+        url: "/questions/9/choices/66",
+        choice: "Coffee",
+        votes: 1
+      },
+      {
+        url: "/questions/9/choices/68",
+        choice: "Hot Chocolate",
+        votes: 1
+      }
+    ]
+  },
+  {
+    url: "/questions/8",
+    published_at: "2015-05-27T21:22:26.619000+00:00",
+    question: "Transport of choice?",
+    choices: [
+      {
+        url: "/questions/8/choices/56",
+        choice: "✈️",
+        votes: 2
+      },
+      {
+        url: "/questions/8/choices/57",
+        choice: "🚁",
+        votes: 1
+      },
+      {
+        url: "/questions/8/choices/62",
+        choice: "🚃",
+        votes: 1
+      },
+      {
+        url: "/questions/8/choices/60",
+        choice: "🚎",
+        votes: 1
+      },
+      {
+        url: "/questions/8/choices/58",
+        choice: "🚀",
+        votes: 0
+      },
+      {
+        url: "/questions/8/choices/61",
+        choice: "🚈",
+        votes: 0
+      },
+      {
+        url: "/questions/8/choices/59",
+        choice: "🚗",
+        votes: 0
+      },
+      {
+        url: "/questions/8/choices/64",
+        choice: "🚠",
+        votes: 0
+      },
+      {
+        url: "/questions/8/choices/63",
+        choice: "⛵️",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/7",
+    published_at: "2015-05-27T21:22:26.601000+00:00",
+    question: "Bacon?",
+    choices: [
+      {
+        url: "/questions/7/choices/55",
+        choice: "🇨🇦",
+        votes: 0
+      },
+      {
+        url: "/questions/7/choices/53",
+        choice: "🇬🇧",
+        votes: 0
+      },
+      {
+        url: "/questions/7/choices/54",
+        choice: "🇺🇸",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/6",
+    published_at: "2015-05-27T21:22:26.576000+00:00",
+    question: "Favourite colour?",
+    choices: [
+      {
+        url: "/questions/6/choices/51",
+        choice: "Blue",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/50",
+        choice: "Cyan",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/49",
+        choice: "Green",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/47",
+        choice: "Orange",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/46",
+        choice: "Red",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/52",
+        choice: "Violet",
+        votes: 0
+      },
+      {
+        url: "/questions/6/choices/48",
+        choice: "Yellow",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/5",
+    published_at: "2015-05-27T21:22:26.557000+00:00",
+    question: "Console of choice?",
+    choices: [
+      {
+        url: "/questions/5/choices/44",
+        choice: "Wii U",
+        votes: 1
+      },
+      {
+        url: "/questions/5/choices/43",
+        choice: "PlayStation 4",
+        votes: 0
+      },
+      {
+        url: "/questions/5/choices/45",
+        choice: "Xbox One",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/4",
+    published_at: "2015-05-27T21:22:26.512000+00:00",
+    question: "Best fruit?",
+    choices: [
+      {
+        url: "/questions/4/choices/39",
+        choice: "🍌",
+        votes: 1
+      },
+      {
+        url: "/questions/4/choices/32",
+        choice: "🌽",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/24",
+        choice: "🍅",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/28",
+        choice: "🍆",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/38",
+        choice: "🍇",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/25",
+        choice: "🍈",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/29",
+        choice: "🍉",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/33",
+        choice: "🍊",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/36",
+        choice: "🍋",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/26",
+        choice: "🍍",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/30",
+        choice: "🍎",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/34",
+        choice: "🍏",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/37",
+        choice: "🍐",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/40",
+        choice: "🍑",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/27",
+        choice: "🍒",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/42",
+        choice: "🍒",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/31",
+        choice: "🍓",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/41",
+        choice: "🍓",
+        votes: 0
+      },
+      {
+        url: "/questions/4/choices/35",
+        choice: "🍠",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/3",
+    published_at: "2015-05-27T21:22:26.486000+00:00",
+    question: "Favourite tea type?",
+    choices: [
+      {
+        url: "/questions/3/choices/17",
+        choice: "Black Tea",
+        votes: 1
+      },
+      {
+        url: "/questions/3/choices/18",
+        choice: "Green Tea",
+        votes: 0
+      },
+      {
+        url: "/questions/3/choices/23",
+        choice: "Herbal",
+        votes: 0
+      },
+      {
+        url: "/questions/3/choices/20",
+        choice: "Matcha",
+        votes: 0
+      },
+      {
+        url: "/questions/3/choices/19",
+        choice: "Oolong Tea",
+        votes: 0
+      },
+      {
+        url: "/questions/3/choices/22",
+        choice: "Pu-erh",
+        votes: 0
+      },
+      {
+        url: "/questions/3/choices/21",
+        choice: "White Tea",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/2",
+    published_at: "2015-05-27T21:22:26.457000+00:00",
+    question: "Who is the best Avenger?",
+    choices: [
+      {
+        url: "/questions/2/choices/8",
+        choice: "Iron Man",
+        votes: 2
+      },
+      {
+        url: "/questions/2/choices/12",
+        choice: "Black Widow",
+        votes: 1
+      },
+      {
+        url: "/questions/2/choices/9",
+        choice: "Thor",
+        votes: 1
+      },
+      {
+        url: "/questions/2/choices/11",
+        choice: "Captain America",
+        votes: 0
+      },
+      {
+        url: "/questions/2/choices/13",
+        choice: "Hawkeye",
+        votes: 0
+      },
+      {
+        url: "/questions/2/choices/10",
+        choice: "Hulk",
+        votes: 0
+      },
+      {
+        url: "/questions/2/choices/16",
+        choice: "Scarlet Witch",
+        votes: 0
+      },
+      {
+        url: "/questions/2/choices/14",
+        choice: "Vision",
+        votes: 0
+      },
+      {
+        url: "/questions/2/choices/15",
+        choice: "War Machine",
+        votes: 0
+      }
+    ]
+  },
+  {
+    url: "/questions/1",
+    published_at: "2015-05-27T21:22:26.431000+00:00",
+    question: "Favourite programming language?",
+    choices: [
+      {
+        url: "/questions/1/choices/1",
+        choice: "Swift",
+        votes: 41
+      },
+      {
+        url: "/questions/1/choices/7",
+        choice: "JavaScript",
+        votes: 33
+      },
+      {
+        url: "/questions/1/choices/2",
+        choice: "Python",
+        votes: 5
+      },
+      {
+        url: "/questions/1/choices/5",
+        choice: "C",
+        votes: 4
+      },
+      {
+        url: "/questions/1/choices/3",
+        choice: "Objective-C",
+        votes: 2
+      },
+      {
+        url: "/questions/1/choices/6",
+        choice: "C++",
+        votes: 1
+      },
+      {
+        url: "/questions/1/choices/4",
+        choice: "Ruby",
+        votes: 1
+      }
+    ]
+  }
+];
