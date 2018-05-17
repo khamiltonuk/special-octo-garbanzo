4b0412560f7205fa2c294da55fd598a45a671234
khamiltonuk <krisham87@hotmail.com>
2018-05-17 08:32:20 +0200

    creat action functions

---
+++ src/actions/actions.js
@@ -0,0 +1,22 @@
+import * as types from "./../actions/actions";
+
+export function fetchQuestions(payload) {
+  return {
+    type: types.FETCH_QUESTIONS,
+    payload
+  };
+}
+
+export function fetchQuestionsSuccess(payload) {
+  return {
+    type: types.FETCH_QUESTIONS_SUCCESS,
+    payload
+  };
+}
+
+export function fetchQuestionsError(payload) {
+  return {
+    type: types.FETCH_QUESTIONS_ERROR,
+    payload
+  };
+}
